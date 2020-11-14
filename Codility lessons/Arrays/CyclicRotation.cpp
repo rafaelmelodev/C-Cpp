f4d75ceaee721ef2722ee95923f8af4dc79a2627
Rafael Santos <rafaelsantos.ee90@gmail.com>
2020-11-13 23:57:06 -0300

    Cyclic rotation

---
+++ Codility lessons/Arrays/CyclicRotation.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> &A, int K) {
+
+    // If vector is empty, return an empty vector
+    if (A.size() == 0) {
+        return {};    
+    }
+    
+    for (int i = 0; i < K; i++) {
+        A.insert(A.begin(), A.back());
+        A.pop_back();
+    }
+    
+    return A;
+}
+
+int main(){
+
+    auto il1 = {2,3,5,6,7};
+    vector<int> A(il1), result;
+    int K = 4;
+
+    cout << "Before rotating: " ;
+    for (int i = 0; i < A.size(); ++i)
+        cout << A[i] << " ";
+
+    result = solution(A, K);
+
+    cout << "\nAfter rotating " << K << " times: ";
+    for (int i = 0; i < result.size(); ++i)
+        cout << result[i] << " ";
+
+    return 0;
+}
